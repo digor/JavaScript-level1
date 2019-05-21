af27f88fc944a0eb51ada2983f7fc7af0039bc55
digor <idabija@gmail.com>
2019-05-21 21:14:41 +0300

    create index.html finish homework )

--- js/lesson2.js
+++ js/lesson2.js
@@ -1,4 +1,3 @@
-var gamerOpt;
 function game (gamerOpt){
 
     var pcOpt = Math.round (Math.random () * 2 ) + 1;
@@ -9,11 +8,12 @@ function game (gamerOpt){
         document.getElementById("resultGame").innerHTML = "<h2 style='color:red;'>Вы програли</h2>";
     }
 }
+
 function task3(){
-    var a = prompt('a = ');
-    var b = prompt('b = ');
-    a = parseInt(a);
-    b = parseInt(b);
+    var a = parseInt(document.getElementById("a").value);
+    var b = parseInt(document.getElementById("b").value);
+    var x;
+
     if (a > 0 && b > 0){
         x = a - b;
     } else if (a < 0 && b < 0){
@@ -23,22 +23,21 @@ function task3(){
     }
     document.getElementById("resTask3").innerHTML = "x = " + x;
 }
+
 function task4() {
-    var a = prompt('Введите число от 1 до 15');
-    a = parseInt(a);
+    var a = parseInt(document.getElementById("t4").value);
 
-    if (a > 0 && a <=15) {
-
+    if (a >= 0 && a <=15) {
         var x;
 
         switch (a) {
             case 1:
                 x = x + '1';
-            case 1:
-                x = x + ' 1';
             case 2:
                 x = x + ' 2';
             case 3:
+                x = x + ' 3';
+            case 4:
                 x = x + ' 4';
             case 5:
                 x = x + ' 5';
@@ -68,14 +67,44 @@ function task4() {
         alert('Только цифры от 0 до 15');
     }
 }
-function task5(){
-    var a = prompt('a = ');
-    var b = prompt('b = ');
-    a = parseInt(a);
-    b = parseInt(b);
-    var operation = prompt('Выберите тип операции:\n' +
-        '1. + \n' +
-        '2. - \n' +
-        '3. * \n' +
-        '4. /');
+
+function addition (a, b){
+    return document.getElementById("resTask5").innerHTML = "Сумма: " + (a + b);
+}
+
+function minus (a, b){
+    return document.getElementById("resTask5").innerHTML = "Вычитание: " + (a - b);
+}
+
+function multipl (a, b){
+    return document.getElementById("resTask5").innerHTML = "Умножение: " + (a * b);
+}
+
+function division (a, b){
+    return document.getElementById("resTask5").innerHTML = "Деление: " + (a / b);
+}
+
+function mathOperation (a, b, operation){
+    switch (operation){
+        case 1:
+            addition (a, b);
+            break;
+        case 2:
+            minus (a, b);
+            break;
+        case 3:
+            multipl (a, b);
+            break;
+        case 4:
+            division (a, b);
+            break;
+    }
+}
+
+function power (val, pow) {
+    var res = val;
+    if (pow > 1) {
+        res = val * power(val, --pow);
+    }
+    return document.getElementById("resTask8").innerHTML = + res;
 }
