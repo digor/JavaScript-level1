fb2478a1031254b7dd2e49b3a2a86e2fb82fa516
digor <idabija@gmail.com>
2019-05-22 22:52:27 +0300

    make task4 and task5

---
+++ js/lesson3.js
@@ -0,0 +1,12 @@
+function task4() {
+    for (var i = 0; i <= 9; i++){
+        console.log (i);
+    }
+}
+function task5() {
+    var piramid ;
+    for (var i = 1; i <= 20; i++){
+        piramid +='x';
+        console.log(piramid);
+    }
+}
