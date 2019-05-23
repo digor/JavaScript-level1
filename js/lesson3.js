fb7773a15e804b90737ad0ae5249578fd0871a3b
digor <idabija@gmail.com>
2019-05-23 17:37:39 +0300

    finis lesson3

--- js/lesson3.js
+++ js/lesson3.js
@@ -1,13 +1,27 @@
 function task1() {
-    var a = true;
-    while (a) {
-
+    var arr= [];
+    var nr = 0;
+    while (nr <= 100) {
+        var prNr = false;
+        if (nr == 2) {
+            arr.push(nr);
+        }
+        for (var i = 2; i <= nr; i++){
+            if (nr % i == 0) {
+                break;
+            } else {
+                prNr = true;
+            }
+        }
+        if (prNr){
+            arr.push(nr);
+        }
+        nr++;
     }
+    console.log (arr);
 }
 function task4() {
-    for (var i = 0; i <= 9; i++){
-        console.log (i);
-    }
+    for (var i = 0; i <= 9; console.log (i++)){}
 }
 function task5() {
     var piramid ;
