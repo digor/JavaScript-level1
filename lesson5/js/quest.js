6b6d1306d12af241ccde4d2e4e59be66a572d2b4
digor <idabija@gmail.com>
2019-05-30 21:06:56 +0300

    work at game quest )

--- lesson5/js/quest.js
+++ lesson5/js/quest.js
@@ -1,13 +1,43 @@
-var steps = 0, question, answ;
+var steps = 0, question;
 var makedMoves = [];
+
+var elGame = {
+    btnStGame: document.querySelector('.stGame'),
+    btnExitGame: document.querySelector('.exitGame'),
+    txtAction: document.querySelector('.textAction'),
+    action: document.querySelector('.action')
+};
 
 function stGame(n) {
     question = works[n];
 
     let st = (steps !== 0) ? 'шаги: ' + steps : '';
 
-    answ = +prompt(question.text + '\n' + selectMoves(n) + '\n ' + st);
+    // show hidden elements of game
+    elGame.btnStGame.classList.add('hideBlock');
+    elGame.btnExitGame.classList.remove('hideBlock');
+    elGame.txtAction.classList.remove('hideBlock');
+    elGame.action.classList.remove('hideBlock');
 
+    elGame.txtAction.innerText = question.text;
+
+    var newDiv = document.createElement('div');
+    newDiv.innerHTML = 'some text';
+    elGame.action.appendChild(selectMoves(n));
+    /*newDiv.innerHTML = 'some text2';
+    elGame.action.insertAdjacentHTML('beforeend', newDiv);
+    newDiv.innerHTML = 'some text3';
+    elGame.action.insertAdjacentHTML('beforeend', newDiv);*/
+
+    let ms = selectMoves(n);
+    alert(ms);
+    action.appendChild(ms);
+
+    //answ = +prompt(question.text + '\n' + selectMoves(n) + '\n ' + st);
+
+}
+
+function getAnsw(answ) {
     if (answ === 1111) {
         total();
     } else if (answ === 1 || answ === 2) {
@@ -30,12 +60,16 @@ function total() {
 }
 
 function selectMoves(n) {
-    let moves = '';
+    let moves = document.createElement("template");
+
+    let el = document.createElement('div');
 
     for (let key in question.answer) {
-        moves += question.answer[key];
+
+        el.innerText = question.answer[key];
+        moves += el;
     }
-    moves += '1111 - Выйти из игры';
+
     return moves;
 }
 
